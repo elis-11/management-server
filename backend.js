6688e7be519de8710d6d501f5f65913eafb7f572
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-20 13:37:16 +0100

    reparatur test.rest

---
+++ backend.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const app = express();
+const port = 3022;
+
+const mongoConnectString = 'mongodb://localhost:27017';
+const client = new MongoClient(mongoConnectString);
+
+const getData = async (done) => {
+	await client.connect();
+	const db = client.db('api002');
+	done(db);
+};
+
+app.get('/', (req, res) => {
+	getData(async (db) => {
+		const users = await db.collection('users100').find().toArray();
+		res.json(users);
+	});
+});
+
+app.listen(port, () => {
+	console.log(`listening on port http://localhost:${port}`);
+});
