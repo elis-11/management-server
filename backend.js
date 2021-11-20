d39ab506ce3042e27686230b884ed9bdf60a850c
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-20 15:37:14 +0100

    test.rest again

--- backend.js
+++ backend.js
@@ -7,6 +7,8 @@ const port = 3022;
 const mongoConnectString = 'mongodb://localhost:27017';
 const client = new MongoClient(mongoConnectString);
 
+app.use(express.json());
+
 const execMongo = async (done) => {
 	await client.connect();
 	const db = client.db('api002');
@@ -15,25 +17,38 @@ const execMongo = async (done) => {
 
 app.get('/', (req, res) => {
 	execMongo(async (db) => {
-		const users = await db.collection('users100').find()
-        .project({
-            name: 1,
-            username: 1,
-            email: 1
-        }).toArray();
+		const users = await db
+			.collection('users100')
+			.find()
+			.project({
+				name: 1,
+				username: 1,
+				email: 1
+			})
+			.toArray();
 		res.json(users);
 	});
 });
 
 app.delete('/deleteuser/:id', (req, res) => {
-    const id= req.params.id
-    execMongo(async (db) => {
-        const deleteResult = await db.collection('users100').deleteOne({ _id: new mongodb.ObjectId(id) });
-        res.json({
-            result: deleteResult
-        });
-    });
-})
+	const id = req.params.id;
+	execMongo(async (db) => {
+		const deleteResult = await db.collection('users100').deleteOne({ _id: new mongodb.ObjectId(id) });
+		res.json({
+			result: deleteResult
+		});
+	});
+});
+
+app.post('/adduser', (req, res) => {
+	const user = req.body.user;
+	execMongo(async (db) => {
+		const insertResult = await db.collection('users100').insertOne(user);
+		res.json({
+			result: insertResult
+		});
+	});
+});
 
 app.listen(port, () => {
 	console.log(`listening on port http://localhost:${port}`);
